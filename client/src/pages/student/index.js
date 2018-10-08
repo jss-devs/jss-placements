db61b8378e0c14aa127e600f074b5f48f980d38d
Aditya Agarwal <adityaa803@gmail.com>
2018-10-08 15:07:23 +0530

    feature made

--- client/src/pages/student/index.js
+++ client/src/pages/student/index.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from "react";
 import { Route, Link } from "react-router-dom";
 
+import { CheckMobile } from '../../components/CheckMobile';
 import { Notices } from './notices';
 import { Notifs } from './notifs';
 
 const StudentCommon = () => (
   <div>
     <h1>Student Common</h1>
+    <CheckMobile
+      yes={
+        <h2>On Mobile</h2>
+      }
+      no={
+        <h1>On Desktop</h1>
+      }
+    />
     <Link to="/student/notices">Notices</Link>
     <Link to="/student/notifs">Notifs</Link>
   </div>
