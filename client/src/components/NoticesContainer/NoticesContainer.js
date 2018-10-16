7ccb725ea82719531cec1361f36c6fe6ed797df4
Aditya Agarwal <adityaa803@gmail.com>
2018-10-17 00:50:21 +0530

    mock-api and ajax added (#22)

--- client/src/components/NoticesContainer/NoticesContainer.js
+++ client/src/components/NoticesContainer/NoticesContainer.js
@@ -1,71 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import request from 'superagent';
 
 import classes from './NoticesContainer.module.css';
 import { JobCard } from '../JobCard';
 
-const notices = [
-  {
-    companyName: 'Adobe',
-    post: 'React Developer',
-    salary: '5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'selected'
-  },
-  {
-    companyName: 'GradeUp',
-    post: 'Node Developer',
-    salary: '5.5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'pending'
-  },
-  {
-    companyName: 'Adobe',
-    post: 'React Developer',
-    salary: '5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'selected'
-  },
-  {
-    companyName: 'GradeUp',
-    post: 'Node Developer',
-    salary: '5.5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'rejected'
-  },
-  {
-    companyName: 'Adobe',
-    post: 'React Developer',
-    salary: '5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'selected'
-  },
-  {
-    companyName: 'GradeUp',
-    post: 'Node Developer',
-    salary: '5.5 LPA',
-    city: 'Noida',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.',
-    status: 'pending'
+class NoticesContainer extends Component {
+  state = {
+    notices: []
   }
-];
 
-const NoticesContainer = () => (
-  <section className={classes.noticesContainer}>
-    {notices.map(notice => (
-      <JobCard className={classes.noticeCard} {...notice} />
-    ))}
-  </section>
-);
+  requests = []
+
+  dataMapper(notices) {
+    const mapping = {
+      'company_name': 'companyName',
+      'job_profile': 'jobProfile'
+    }
+
+    notices.forEach(notice => {
+      for(let key in mapping) {
+        const newKey = mapping[key];
+        notice[newKey] = notice[key];
+      }
+    });
+
+    return notices;
+  }
+
+  async componentDidMount() {
+    try {
+      const req = request.get('http://localhost:5000/notices');
+      this.requests.push(req);
+      const res = await req;
+      const notices = this.dataMapper(res.body);
+      this.setState({ notices });
+    } catch (error) {
+      console.log('Error Occurred', error);
+    }
+  }
+
+  componentWillUnmount() {
+    this.requests.forEach(req => req.abort());
+  }
+
+  render() {
+    return (
+      <section className={classes.noticesContainer}>
+        {this.state.notices.map(notice => (
+          <JobCard key={notice.id} className={classes.noticeCard} {...notice} />
+        ))}
+      </section>
+    )
+  }
+}
 
 export { NoticesContainer };
